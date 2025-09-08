4cb5bc9d8cbd79d3b5d5d6c8b98873bf2df3eb9a
NikolozDarsalia <nikadarsalia@gmail.com>
2025-09-08 12:49:25 +0200

    adding new files

---
+++ temperature.py
@@ -0,0 +1,13 @@
+def c_to_f(celsiuses):
+    fahrenheits = []
+    for celsius in celsiuses:
+        if type(celsius) not in [int, float]:
+            raise ValueError('Only numbers are accepted')
+        
+        fahrenheit = celsius * 9/5 + 32
+        fahrenheits.append(fahrenheit)
+
+    return fahrenheits
+
+print(c_to_f([5]))
+
